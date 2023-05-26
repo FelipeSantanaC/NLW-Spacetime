af1877bd2df7931f4f7d8396e76739e4721d6c53
FelipeSantanaC <felipecabocloo@gmail.com>
2023-05-26 07:52:54 -0300

    Versão final

---
+++ web/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'avatars.githubusercontent.com', '10.0.0.106',
+        ],
+    },
+}
+
+module.exports = nextConfig
